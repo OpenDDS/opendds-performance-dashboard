83f659996ba6ff2de9c5cc1de56baef6caf94958
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-09-08 15:22:37 -0500

    added ESLint and Prettier to server part

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -3,8 +3,20 @@
   import LineChart from './LineChart.svelte';
 
   const BASE_URL = 'http://localhost:1919/';
+  const DATA_NAME = 'echo_rtps';
+  const STAT_TYPE = 'Latency';
+
+  const axis = {
+    x: {
+      type: 'timeseries',
+      tick: {
+        format: '%Y-%m-%d %H:%M:%S' // how displayed
+      }
+    }
+  };
 
   let data = {
+    /*
     xs: {
       data1: 'x1',
       data2: 'x2'
@@ -15,9 +27,12 @@
       ['data1', 30, 200, 100, 400, 150, 250],
       ['data2', 20, 180, 240, 100, 190]
     ]
+    */
+    x: 'x',
+    columns: []
   };
 
-  onMount(() => getChartData('echo_rtps', 'Latency'));
+  onMount(() => getChartData(DATA_NAME, STAT_TYPE));
 
   async function getChartData(name, statType) {
     const url = BASE_URL + name + '/' + statType;
@@ -28,21 +43,28 @@
       console.log('App.svelte getChartData: rawData =', rawData);
       const timestamps = Object.keys(rawData);
       const xValues = timestamps.map(timestamp => timestamp.split('+')[0]);
-      console.log('App.svelte x: xValues =', xValues);
       const [firstKey] = timestamps;
-      console.log('App.svelte x: firstKey =', firstKey);
       const dataNames = Object.keys(rawData[firstKey][name]);
-      console.log('App.svelte x: dataNames =', dataNames);
-      const columns = [];
+
+      const arr = ['x'];
+      for (let i = 0; i < dataNames.length; i++) {
+        //arr.push(i);
+        arr.push(xValues[i]);
+      }
+      const columns = [arr];
+
       for (const dataName of dataNames) {
-        const column = [];
+        const column = [dataName];
         for (const timestamp of timestamps) {
           const dataForName = rawData[timestamp][name];
           if (dataForName) column.push(dataForName[dataName].mean);
         }
         columns.push(column);
       }
-      console.log('App.svelte x: columns =', columns);
+
+      //data = {xFormat: '%Y-%m-%dT%H:%M:%S', x: 'x', columns};
+      data = {x: 'x', columns};
+      console.log('App.svelte x: data =', data);
     } catch (e) {
       console.error(e);
       alert(e.message);
@@ -51,5 +73,5 @@
 </script>
 
 <main>
-  <LineChart {data} title="Sample Chart" />
+  <LineChart {axis} bind:data title={DATA_NAME + ' ' + STAT_TYPE} />
 </main>
--- ui/src/LineChart.svelte
+++ ui/src/LineChart.svelte
@@ -2,11 +2,14 @@
   import c3 from 'c3';
   import {onMount} from 'svelte';
 
+  export let axis;
   export let data;
   export let title;
 
+  $: c3.generate({bindto: '#chart', data});
+
   onMount(() => {
-    c3.generate({bindto: '#chart', data});
+    c3.generate({axis, bindto: '#chart', data});
   });
 </script>
 
