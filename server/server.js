e78304dd8c3e757c3171ec7e2b900e984b6d6892
Eldon Ahrold <eldon.ahrold@gmail.com>
2021-06-24 16:46:37 -0500

    Update server to mimic new data structure.

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const {getData} = require('./scrape');
 const app = express();
 app.use(cors());
 
+const isDev = process.argv[process.argv.length - 1] === '--dev';
+
 // This serves a JSON file that was saved for test purposes.
 app.use(express.static(path.resolve(__dirname, 'public')));
 
@@ -17,11 +19,55 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+
+// This retrieves the JSON data at a specific URL.
+// It is needed to work around CORS issues
+// because the client cannot directly fetch this URL.
+app.get('/bench2/timestamps.json', async (req, res) => {
+  if(isDev) {
+    const json = require('./public/open-dds-commits.json')
+    return res.json(json)
+  }
+  fetchJson('http://scoreboard.ociweb.com/bench2/scrape_output.json', res);
+});
+
+// This retrieves the JSON data at a specific URL.
+// It is needed to work around CORS issues
+// because the client cannot directly fetch this URL.
+app.get('/bench2/scrape_output.json', async (req, res) => {
+  if(isDev) {
+    const json = require('./public/open-dds-statistics.json')
+    return res.json(json)
+  }
+  fetchJson('http://scoreboard.ociweb.com/bench2/scrape_output.json', res);
+});
+
+// This retrieves the JSON data at a specific URL.
+// It is needed to work around CORS issues
+// because the client cannot directly fetch this URL.
+app.get('/bench2/stat-properties.json', async (req, res) => {  
+  if(isDev) {
+    const json = require('./public/stat-properties.json')
+    console.log(json)
+    return res.json(json)
+  }    
+  fetchJson('http://scoreboard.ociweb.com/bench2/stat_properties.json', res);
+});
+
+app.get('/bench2/raw/:timestamp/results.json', async (req, res) => {
+  const {timestamp} = req.params
+  console.log({timestamp, query: req.params})
+  if(isDev) {
+    return fetchJson(`http://localhost:${PORT}/data/${timestamp}/results.json`, res);
+  }
+  fetchJson(`http://scoreboard.ociweb.com/bench2/raw/${timestamp}/results.json`, res);
+});
+
 // This scrapes data for a specific scenario and statistics type
 // from HTML pages at runtime.
 // An example value for scenario is 'echo_rtps'.
 // An example value for statType is 'Latency'.
-app.get('/:scenario/:statType', async (req, res) => {
+app.get('/data/:scenario/:statType', async (req, res) => {
   const {scenario, statType} = req.params;
   const data = await getData(scenario, statType);
   res.send(JSON.stringify(data));
@@ -40,24 +86,6 @@ async function fetchJson(url, res) {
     res.status(500).end(e.message);
   }
 }
-
-// This retrieves the JSON data at a specific URL.
-// It is needed to work around CORS issues
-// because the client cannot directly fetch this URL.
-app.get('/data', async (req, res) => {
-  const url = 'http://scoreboard.ociweb.com/bench2/scrape_output.json';
-  //const url = 'http://localhost:1919/open-dds-statistics.json';
-  fetchJson(url, res);
-});
-
-// This retrieves the JSON data at a specific URL.
-// It is needed to work around CORS issues
-// because the client cannot directly fetch this URL.
-app.get('/stat-properties', async (req, res) => {
-  const url = 'http://scoreboard.ociweb.com/bench2/stat_properties.json';
-  //const url = 'http://localhost:1919/stat-properties.json';
-  fetchJson(url, res);
-});
 
 const PORT = 1919;
 app.listen(PORT, () => console.info('browse http://localhost:' + PORT));
