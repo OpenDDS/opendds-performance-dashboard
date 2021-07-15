5434a61f04981b2736296ec9b097e28472709982
Eldon Ahrold <eldon.ahrold@gmail.com>
2021-07-15 10:32:02 -0500

    Fix Vite config to be relative to directory root.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import {defineConfig} from 'vite';
+import {svelte} from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     svelte({
       preprocess: sveltePreprocess({
         postcss: {
-          plugins: [require("autoprefixer")()],
-        },
-      }),
-    }),
-  ],
+          plugins: [require('autoprefixer')()]
+        }
+      })
+    })
+  ]
 });
